567bef53a229ae8b8533d4256357ede1a530c921
Barnabas Bulpett <bbulpett@charter.net>
2016-06-21 22:29:00 -0400

    Add seed data to items route

--- app/routes/items.js
+++ app/routes/items.js
@@ -1,4 +1,25 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+	model() {
+		let data = [
+			{
+				"id": 		"1",
+				"name": 	"Tammy"
+			},
+			{
+				"id": 		"2",
+				"name": 	"Lenny"
+			},
+			{
+				"id": 		"3",
+				"name": 	"Maria"
+			},
+			{
+				"id": 		"4",
+				"name": 	"Jose"
+			},
+		];
+		return data
+	}
 });
